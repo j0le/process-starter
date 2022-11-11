366a7ada89e5159286be312df02256971d7c5130
Jan Ole Hüser <ole@example.com>
2022-11-11 01:38:52 +0100

    start a process via 2 stages of process-starter.exe
    Do this via a batch script

--- copy-exe-to-vm.sh
+++ copy-exe-to-vm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # for git bash or cygwin’s bash or similar
+set -e
 
 cp 'C:\Users\ols3\prog\process-starter\x64\Debug\process-starter.exe' '\\windev2208eval\Freigabe\'
+cp 'C:\Users\ols3\prog\process-starter\krass.cmd' '\\windev2208eval\Freigabe\'
---
+++ krass.cmd
@@ -0,0 +1,1 @@
+C:\Freigabe\process-starter.exe --dup-token yes --pid 2644  --program-name C:\Windows\System32\cmd.exe --cmd-line "\"C:\Windows\System32\cmd.exe\" /k (C:\Freigabe\process-starter.exe --wt-session active --pid 9584 --program-name C:\Windows\System32\cmd.exe --cmd-line \"C:\Windows\System32\cmd.exe /k echo hallo\" )"
